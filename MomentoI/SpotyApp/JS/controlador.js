6a198dc23978ed78b0deaef536f7977a23d28344
tamayoram <61298214+tamayoram@users.noreply.github.com>
2020-09-23 23:29:41 -0500

    Conexión con la API de Spotify

--- MomentoI/SpotyApp/JS/controlador.js
+++ MomentoI/SpotyApp/JS/controlador.js
@@ -1,29 +1,29 @@
-/*Preparado para iniciar*/
 
-let titulo=document.getElementById("titulo");
-let reproductor=document.getElementById("audio");
-let imagen=document.getElementById("imagen");
-let boton1=document.getElementById("pista1");
-let boton2=document.getElementById("pista2");
+//url que se trae de Spotify
+const url="https://api.spotify.com/v1/artists/4PhdYoQGH8s1xee81dqQOO/top-tracks?country=US";
 
-// Se chequea en la consola del navegador en el inspeccionar. 
-console.log(titulo);
+//Corresponde al token generados por Spotify
+const token="Bearer BQARcpzS4tvNfXUBj62WECYhY8EOLiOOTIHIPcAL6-_l5-kqtOk18tf-Pe1zeuUySuyF5ReKe4a3YwAPCXtzIW1vahEmca1E0_Gfy5gU0wzlVncoDX55I_MxvyBJQHr-Y0H6ydPywA";
 
-//Instrucciones para cambiar un texto
-titulo.textContent="Coldplay";
+//Parámetros de la petición
 
-// Utilización del evento click
+const parametros={
+
+method:"GET",
+headers:{"Authorization":token}
+
+};
+
+// Fetch permite realizar la función de clientes para realizar el llamado de la información que se requiere
+fetch(url,parametros)
+    .then(respuesta=>respuesta.json())
+    .then(datos=>depurarDatos(datos));
 
 
-boton1.addEventListener("click",CambiarPista1);
-boton2.addEventListener("click",CambiarPista2);
-
-function CambiarPista1(){
-
-    reproductor.src="recursos/audio1.mp3";
-}
-
-function CambiarPista2(){
-
-    reproductor.src="recursos/audio2.mp3";
-}
+function depurarDatos(datos){
+    console.log(datos);
+    console.log(datos.tracks);
+    console.log(datos.tracks[0]);
+    console.log(datos.tracks[6].preview_url);
+    console.log(datos.tracks[6].name);
+};
