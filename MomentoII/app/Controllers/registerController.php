b2c0b511501491611caead340bb597b4c4177ef7
tamayoram <61298214+tamayoram@users.noreply.github.com>
2020-10-28 19:47:43 -0500

    Primeros pasos momentoII

---
+++ MomentoII/app/Controllers/registerController.php
@@ -0,0 +1,12 @@
+<?php namespace App\Controllers;
+
+class registerController extends BaseController
+{
+	public function index()
+	{
+		return view('registerView');
+	}
+
+	//--------------------------------------------------------------------
+
+}
