5708036aa7c1192d0782b608a45870f1d88f49fd
tamayoram <61298214+tamayoram@users.noreply.github.com>
2020-11-09 21:45:50 -0500

    Avance en momento II consulta cards BD

--- MomentoII/app/Views/listView.php
+++ MomentoII/app/Views/listView.php
@@ -1,12 +1,26 @@
 <?php include_once('header.php'); ?>
 
 
+<div class="row row-cols-1 row-cols-md-3 mt-4">
 
-    <?php
-
-    print_r($animals);
+    <?php foreach ($animals as $animal) : ?>
     
-    ?>
+        <div class="col mb-4">
+            <div class="card h-100">
+                <img src="https://placeimg.com/640/280/animals" class="card-img-top" alt="Imagen de animal">
+                <div class="card-body">
+                    <h5 class="card-title">Nombre: <?php echo($animal["name"])?></h5>
+                    <p class="card-text">Edad: <?php echo($animal["age"])?> </p>
+                    <p class="card-text">Tipo: <?php echo($animal["type"])?> </p>
+                    <p class="card-text">Descripción: <?php echo($animal["description"])?> </p>
+                    <p class="card-text">Comida: <?php echo($animal["food"])?> </p>
+                </div>
+            </div>
+        </div>
+    
+    <?php endforeach ?>
+
+</div>
 
 
 
@@ -14,4 +28,4 @@
 
 
 
-<?php include_once('footer.php'); ?>
+    <?php include_once('footer.php'); ?>
